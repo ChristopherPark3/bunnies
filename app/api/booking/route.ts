8edd12ad9403dc1a4c1243baf97d4a16ba0a5ab9
Christopher Park <christopherjcpark00@gmail.com>
2025-08-25 20:50:56 -0700

    Revert

--- app/api/booking/route.ts
+++ app/api/booking/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { prisma } from "@/utils/prisma";
+import prisma from "@/utils/prisma";
 import { Bunny } from "@/lib/types";
 import { resend } from "@/utils/resend";
 
--- utils/prisma.ts
+++ utils/prisma.ts
@@ -1,10 +1,5 @@
-// utils/prisma.ts
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/lib/generated/prisma";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const prisma = new PrismaClient();
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export default prisma;
