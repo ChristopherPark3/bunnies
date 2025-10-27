a5d3bd362fe135498c662002a6527b4cd1f596d4
Christopher Park <christopherjcpark00@gmail.com>
2025-10-26 23:01:40 -0700

    Fix status

--- app/api/status/route.ts
+++ app/api/status/route.ts
@@ -2,10 +2,11 @@ import prisma from "@/utils/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const isUnderMaintenance = await prisma.configuration.findUnique({
+  const config = await prisma.configuration.findUnique({
     where: {
       id: "MainConfig",
     },
   });
+  const isUnderMaintenance = config?.is_under_maintenance || false;
   return NextResponse.json({ isUnderMaintenance });
 }
